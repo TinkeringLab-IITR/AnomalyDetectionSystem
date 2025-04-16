f721252392241e21681485250e14021c58cc10b1
cypher4802 <tanmay258789@gmail.com>
2025-04-16 13:22:22 +0000

    added home page without ui

--- frontend/frontend/src/app/page.tsx
+++ frontend/frontend/src/app/page.tsx
@@ -1,9 +1,24 @@
-import ProcessStat from '@/components/statss'
+// stats/page.js
+"use client"
+import Link from "next/link"
+import { useWebSocket } from "../app/websockets"
 
-export default function Home() {
-  return(
-    <main className="min-h-screen bg-slate-50 py-8">
-      <ProcessStat />
-    </main>
+export default function StatsIndex() {
+  const { dataByPid } = useWebSocket();
+
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Available PIDs</h1>
+      <ul className="space-y-2">
+        {Object.keys(dataByPid).map(pid => (
+          <li key={pid}>
+            <Link href={`/stats/${pid}`} className="text-blue-600 hover:underline">
+              View Stats for PID {pid}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
+
